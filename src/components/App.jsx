b5cda6d57417fb3a8ad583465f2eedcccacbcd7e
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-03-24 14:27:02 +0100

    Add scroll-to-top button and corresponding styles.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,28 @@ import Navbar from "./Navbar";
 import Skills from "./Skills/Skills";
 import Profile from "./Profile/Profile";
 import Projects from "./Projects/Projects";
+import { FaArrowCircleUp } from "react-icons/fa";
 
 export default function App() {
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = () => {
+    scrollFunction();
+  };
+
+  const scrollFunction = async () => {
+    //Get the scroll button
+    const scrollButton = await document.getElementById("scrollButton");
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+      ? (scrollButton.style.display = "block")
+      : (scrollButton.style.display = "none");
+  };
+
+  // Function to move view area to top of screen on button click
+  const handleScroll = () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  };
+
   return (
     <div className="App">
       <Navbar />
@@ -13,6 +33,9 @@ export default function App() {
       <Profile />
       <Skills />
       <Projects />
+      <button onClick={handleScroll} id="scrollButton">
+        <FaArrowCircleUp width={20} />
+      </button>
       <Contact />
     </div>
   );
